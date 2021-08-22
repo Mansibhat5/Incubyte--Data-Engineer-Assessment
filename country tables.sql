b33c2c1a5558f76ff7ced262d174a86eb8a22de4
Mansi_Bhat <77536447+Mansibhat5@users.noreply.github.com>
2021-08-22 10:15:57 +0530

    Add files via upload

---
+++ country tables.sql
@@ -0,0 +1,71 @@
+create table NYC (
+Customer_name varchar(255),
+Customer_id varchar(18),
+Customer_open_date date ,
+Last_consulted_date date,
+Vaccination_type char(5),
+Doctor_consulted char(255),
+State   char(5),
+Country char(5),
+Post_code int,
+Date_of_birth double,
+Active_customer char(1)
+);
+
+create table USA (
+Customer_name varchar(255),
+Customer_id varchar(18),
+Customer_open_date date ,
+Last_consulted_date date,
+Vaccination_type char(5),
+Doctor_consulted char(255),
+State   char(5),
+Country char(5),
+Post_code int,
+Date_of_birth double,
+Active_customer char(1)
+);
+
+create table IND (
+Customer_name varchar(255),
+Customer_id varchar(18),
+Customer_open_date date ,
+Last_consulted_date date,
+Vaccination_type char(5),
+Doctor_consulted char(255),
+State   char(5),
+Country char(5),
+Post_code int,
+Date_of_birth double,
+Active_customer char(1)
+);
+
+create table PHIL (
+Customer_name varchar(255),
+Customer_id varchar(18),
+Customer_open_date date ,
+Last_consulted_date date,
+Vaccination_type char(5),
+Doctor_consulted char(255),
+State   char(5),
+Country char(5),
+Post_code int,
+Date_of_birth double,
+Active_customer char(1)
+);
+
+
+
+create table AU (
+Customer_name varchar(255),
+Customer_id varchar(18),
+Customer_open_date date ,
+Last_consulted_date date,
+Vaccination_type char(5),
+Doctor_consulted char(255),
+State   char(5),
+Country char(5),
+Post_code int,
+Date_of_birth double,
+Active_customer char(1)
+);
